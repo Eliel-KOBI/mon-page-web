83dad4109532aeb5f7d5dc82d8fcce96118874d0
Eliel-KOBI <elielkobi47@gmail.com>
2025-02-11 12:04:43 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Interaction simple : Bouton dans la section Accueil
+document.getElementById("ctaButton").addEventListener("click", function() {
+    alert("Merci de découvrir mes passions !");
+});
+
+// Interaction sur les cartes de passion
+document.getElementById("passion1").addEventListener("click", function() {
+    alert("Vous avez cliqué sur la passion Technologie !");
+});
+
+document.getElementById("passion2").addEventListener("click", function() {
+    alert("Vous avez cliqué sur la passion Voyages !");
+});
+
+document.getElementById("passion3").addEventListener("click", function() {
+    alert("Vous avez cliqué sur la passion Art !");
+});
